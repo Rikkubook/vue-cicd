38e3f74962fb7d378f6c5e90ade112c06d284555
林佩瑤 <rikkubook@gmail.com>
2021-06-20 17:03:59 +0800

    add:Counter

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Counter />
   </div>
 </template>
+
+<script>
+import Counter from '@/components/Counter.vue'
+
+export default {
+  components: {
+    Counter
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Counter.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ count }}</h1>
+    <button @click="CountFn()">點我加1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    CountFn () {
+      this.count += 1
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
