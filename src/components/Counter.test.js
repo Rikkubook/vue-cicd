fbdcdb1694dc5119d1d4b2fbd25d53c6adc8d408
林佩瑤 <rikkubook@gmail.com>
2021-07-06 22:56:14 +0800

    Add click test

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,8 +1,17 @@
-import { render} from '@testing-library/vue'
+import { render, fireEvent} from '@testing-library/vue'
 import Counter from './Counter.vue'
 
 test('載入時title [當前點擊次數0]',()=>{
-  const { getByTestId  } = render(Counter)
+  const { getByTestId } = render(Counter)
   const title = getByTestId('counterId')
   expect(title.innerHTML).toBe('當前點擊次數0')
+}) 
+
+test('click [當前點擊次數1]', async()=>{
+  const { getByTestId, getByText } = render(Counter)
+
+  const incrementButton = getByText('點我加1') // 拿到文字為
+  await fireEvent.click(incrementButton) // 要改成非同步點擊
+  const title = getByTestId('counterId')
+  expect(title.innerHTML).toBe('當前點擊次2') // 確認
 }) 
